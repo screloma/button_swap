44d01035f0f8e7ea8e67e26fc1a675ca0507adb3
screloma <screloma@gmail.com>
2018-07-14 20:56:36 +0300

    Update sosaka.user.js

--- sosaka.user.js
+++ sosaka.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sosaka
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @downloadURL  https://github.com/screloma/sosaka/raw/master/sosaka.user.js
